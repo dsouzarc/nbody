3a2f935b573c35ffb5f2f4c375ab8f4f25391cd7
Ryan D'souza <dsouzarc@gmail.com>
2014-09-09 13:35:46 -0400

    Pictures/data for planets

---
+++ NBody/Planet.java
@@ -0,0 +1,83 @@
+public class Planet { 
+  
+  public static final double G = 6.67 * Math.pow(10, -11);
+  
+  private final String planetName;
+  private final double initialXVelocity, initialYVelocity;
+  private final double initialX, initialY;
+  private final double mass;
+  
+  private double currentX, currentY;
+  private double velocityX, velocityY;
+  
+  public Planet(final double initialX, final double initialY, final double initialXVelocity, 
+                final double initialYVelocity, final double mass, final String planetName) { 
+    this.planetName = planetName;
+    this.initialX = initialX;
+    this.initialY = initialY;
+    this.initialXVelocity = initialXVelocity;
+    this.initialYVelocity = initialYVelocity;
+    this.mass = mass;
+    
+    this.currentX = initialX;
+    this.currentY = initialY;
+    this.velocityX = initialXVelocity;
+    this.velocityY = initialYVelocity;
+  }
+  
+  public String getName() { 
+    return this.planetName;
+  }
+  
+  public double getInitialX() { 
+    return this.initialX;
+  }
+  
+  public double getInitialY() { 
+    return this.initialY;
+  }
+  
+  public double getMass() { 
+    return this.mass;
+  }
+  
+  public double getInitialXVelocity() { 
+    return this.initialXVelocity;
+  }
+  
+  public double getInitialYVelocity() { 
+    return this.initialYVelocity;
+  }
+  
+  public double getVelocityX() { 
+    return this.velocityX;
+  }
+  
+  public double getVelocityY() { 
+    return this.velocityY;
+  }
+  
+  public void setVelocityX(double newX) { 
+    this.velocityX = newX;
+  }
+  
+  public void setVelocityY(double newY) { 
+    this.velocityY = newY;
+  }
+  
+  public void setX(double newX) { 
+    this.currentX = newX;
+  }
+  
+  public void setY(double newY) { 
+    this.currentY = newY;
+  }
+  
+  public double getX() { 
+    return this.currentX;
+  }
+  
+  public double getY() { 
+    return this.currentY;
+  }
+}
---
+++ NBody/PlanetSimulator.java
@@ -0,0 +1,45 @@
+import java.io.File;
+
+public class PlanetSimulator { 
+  
+  private final int numPlanets;
+  private final double universeRadius;
+  private final Planet[] planets;
+  
+  public PlanetSimulator() { 
+    In fileIn = new In(new File("planets.txt"));
+    
+    this.numPlanets = fileIn.readInt();
+    this.universeRadius = fileIn.readDouble();
+    
+    this.planets = new Planet[numPlanets];
+    
+    for(int i = 0; i < this.numPlanets; i++) { 
+      final double initialX = fileIn.readDouble();
+      final double initialY = fileIn.readDouble();
+      final double initialXVel = fileIn.readDouble();
+      final double initialYVel = fileIn.readDouble();
+      final double mass = fileIn.readDouble();
+      final String planetName = fileIn.readString();
+      this.planets[i] = new Planet(initialX, initialY, initialXVel, initialYVel, mass, planetName);
+    }
+    
+    drawPlanets();
+  }
+  
+  private void drawPlanets() { 
+    StdDraw.setScale(-1 * universeRadius, universeRadius);
+    StdDraw.picture(0, 0, "starfield.jpg");
+    for(Planet planet : planets) { 
+      drawPlanet(planet);
+    }
+  }
+  
+  private void drawPlanet(final Planet thePlanet) { 
+    StdDraw.picture(thePlanet.getX(), thePlanet.getY(), thePlanet.getName());
+  }
+  
+  public static void main(String[] ryan) { 
+    PlanetSimulator theSim = new PlanetSimulator();
+  }
+}
