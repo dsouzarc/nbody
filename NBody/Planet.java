15f0eb5b8b8bab5a71256745ee20fac8f37d53b5
Ryan D'souza <dsouzarc@gmail.com>
2014-09-12 06:31:37 -0400

    Replaced distance calculation and invalid methods

--- NBody/Planet.java
+++ NBody/Planet.java
@@ -31,102 +31,20 @@ public class Planet {
     this.sunMass = sunMass;
   }
   
-  public void setF(double sunX, double sunY) { 
-    this.r = Math.sqrt( Math.pow((sunX - initialX), 2) + Math.pow((sunY - initialY), 2)); //Distance formula from sun center to planet center
+  public void setF(final Point sunPoint) { 
+    this.r = currentPoint.getDistance(sunPoint); //Distance formula from sun center to planet center
     this.F = (G * this.sunMass * this.mass) / Math.pow(r, 2);
   }
   
   public double getF() { 
     return this.F;
   }
-  public double getFx(final double changeX) { 
-    return F * (changeX / r);
-  }
-  
-  public double getFy(final double changeY) { 
-    return F * (changeY / r);
-  }
-  
-  public double getAx(final double fX) { 
-    return fX / mass;
-  }
-  
-  public double getAy(final double fY) { 
-    return fY / mass;
-  }
-  
-  public void calculateNewVelocity(final double aX, final double aY) { 
-    this.velocityX = this.velocityX + changeT * aX;
-    this.velocityY = this.velocityY * changeT * aY;
-  }
-  
-  public void calculateNewPoint() { 
-    this.currentX = this.currentX + changeT * this.velocityX;
-    this.currentY = this.currentY + changeT * this.velocityY;
-  }
-  
-  public void calculateNewPoint(final double changeX, final double changeY) { 
-    final double Fx = getFx(changeX);
-    final double Fy = getFy(changeY);
-    final double Ax = getAx(Fx);
-    final double Ay = getAy(Fy);
-    calculateNewVelocity(Ax, Ay);
-    calculateNewPoint();
-  }
-  
+
   public String getName() { 
     return this.planetName;
   }
   
-  public double getInitialX() { 
-    return this.initialX;
-  }
-  
-  public double getInitialY() { 
-    return this.initialY;
-  }
-  
   public double getMass() { 
     return this.mass;
-  }
-  
-  public double getInitialXVelocity() { 
-    return this.initialXVelocity;
-  }
-  
-  public double getInitialYVelocity() { 
-    return this.initialYVelocity;
-  }
-  
-  public double getVelocityX() { 
-    return this.velocityX;
-  }
-  
-  public double getVelocityY() { 
-    return this.velocityY;
-  }
-  
-  public void setVelocityX(double newX) { 
-    this.velocityX = newX;
-  }
-  
-  public void setVelocityY(double newY) { 
-    this.velocityY = newY;
-  }
-  
-  public void setX(double newX) { 
-    this.currentX = newX;
-  }
-  
-  public void setY(double newY) { 
-    this.currentY = newY;
-  }
-  
-  public double getX() { 
-    return this.currentX;
-  }
-  
-  public double getY() { 
-    return this.currentY;
   }
 }
